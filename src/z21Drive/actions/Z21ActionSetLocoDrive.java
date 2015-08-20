d5270ed3e997e7e200f2527149cd4ead1c696d78
grizeldi <grizeldi@gmail.com>
2015-08-20 12:59:31 +0200

    Finished SetLocoDrive

--- src/z21Drive/actions/Z21ActionSetLocoDrive.java
+++ src/z21Drive/actions/Z21ActionSetLocoDrive.java
@@ -11,12 +11,14 @@ public class Z21ActionSetLocoDrive extends Z21Action{
      * @param locoAddress Address of the loco that this action targets.
      * @param speed New speed value.
      * @param speedStepsID How many speed steps loco has (IDs: 0 = 14 speed steps, 2 = 28 speed steps, 3 = 128 speed steps).
-     * @param direction Which direction the loco should go (1 = forward, 0 = backward).
+     * @param direction Which direction the loco should go (true = forward, false = backward).
      * @throws LocoAddressOutOfRangeException Thrown when loco address is too big or negative.
      */
     public Z21ActionSetLocoDrive(int locoAddress, byte speed, byte speedStepsID, boolean direction) throws LocoAddressOutOfRangeException{
         byteRepresentation.add(Byte.decode("0x40"));
         byteRepresentation.add(Byte.decode("0x00"));
+        if (locoAddress < 1 || locoAddress > 63)
+            throw new LocoAddressOutOfRangeException(locoAddress);
         addDataToByteRepresentation(new Object[]{locoAddress, speed, speedStepsID, direction});
         addLenByte();
     }
@@ -41,5 +43,26 @@ public class Z21ActionSetLocoDrive extends Z21Action{
         byte Adr_LSB = (byte)24;
         byteRepresentation.add(Adr_MSB);
         byteRepresentation.add(Adr_LSB);
+        
+        boolean [] speedAndDirection = fromByte((Byte) objs[1]);
+        speedAndDirection[0] = (Boolean) objs[3];
+        byteRepresentation.add((byte)((speedAndDirection[0]?1<<7:0) + (speedAndDirection[1]?1<<6:0) + (speedAndDirection[2]?1<<5:0) +
+                (speedAndDirection[3]?1<<4:0) + (speedAndDirection[4]?1<<3:0) + (speedAndDirection[5]?1<<2:0) +
+                (speedAndDirection[6]?1<<1:0) + (speedAndDirection[7]?1:0)));
+        byteRepresentation.add((byte) (byteRepresentation.get(2) ^ byteRepresentation.get(3)
+                ^ byteRepresentation.get(4) ^ byteRepresentation.get(5) ^ byteRepresentation.get(6) ^ byteRepresentation.get(7)));
+    }
+
+    private boolean [] fromByte(byte x){
+        boolean bs[] = new boolean[8];
+        bs[0] = ((x & 0x01) != 0);
+        bs[1] = ((x & 0x02) != 0);
+        bs[2] = ((x & 0x04) != 0);
+        bs[3] = ((x & 0x08) != 0);
+        bs[4] = ((x & 0x16) != 0);
+        bs[5] = ((x & 0x32) != 0);
+        bs[6] = ((x & 0x64) != 0);
+        bs[7] = ((x & 0x128) != 0);
+        return bs;
     }
 }
