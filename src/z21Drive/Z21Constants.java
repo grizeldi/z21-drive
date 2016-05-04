fb4fb5b7f1cf9f8433105095ea88a6a9ca527d85
grizeldi <grizeldi@gmail.com>
2016-05-04 19:11:38 +0200

    Added constants

---
+++ src/z21Drive/Z21Constants.java
@@ -0,0 +1,9 @@
+package z21Drive;
+
+public class Z21Constants {
+    /**
+     * Used in combination with loco related actions and broadcasts.
+     * Maps internal ids of speed steps to actual speed steps.
+     */
+    public static final int SPEED_STEPS_14 = 0, SPEED_STEPS_28 = 2, SPEED_STEPS_128 = 3;
+}
