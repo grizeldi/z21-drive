08ba3ec47af07194397020f56f179e44c407a7a3
Sven Bursch-Osewold <git@bursch.com>
2016-06-16 08:33:10 +0200

    Bugfix for TestSerialnumber

--- src/z21Drive/testing/Serialnumber.java
+++ src/z21Drive/testing/Serialnumber.java
@@ -4,6 +4,7 @@ import z21Drive.Z21;
 import z21Drive.actions.Z21ActionGetSerialNumber;
 import z21Drive.responses.ResponseTypes;
 import z21Drive.responses.Z21Response;
+import z21Drive.responses.Z21ResponseGetSerialNumber;
 import z21Drive.responses.Z21ResponseListener;
 
 /**
@@ -12,31 +13,28 @@ import z21Drive.responses.Z21ResponseListener;
  */
 public class Serialnumber implements Runnable{
 
-    public static void main(String[] args) {
-        //Start things up
-        new Thread(new Serialnumber()).start();
-    }
+	public static void main(String[] args) {
+		//Start things up
+		new Thread(new Serialnumber()).start();
+	}
 
-    public void run(){
-        final Z21 z21 = Z21.instance;
-    	Z21ResponseListener b;		            
-    	
-    	{
-    		z21.addResponseListener(new Z21ResponseListener() {
-                @Override
-                public void responseReceived(ResponseTypes type, Z21Response response) {
-                	if (type != ResponseTypes.LAN_GET_SERIAL_NUMBER_RESPONSE){
-                		return;
-                	}
-                	System.out.println("Received Response");
-                }
+	public void run(){
+		final Z21 z21 = Z21.instance;
+		z21.addResponseListener(new Z21ResponseListener() {
+			@Override
+			public void responseReceived(ResponseTypes type, Z21Response response) {
+				if (type != ResponseTypes.LAN_GET_SERIAL_NUMBER_RESPONSE){
+					return;
+				}
+				Z21ResponseGetSerialNumber number = (Z21ResponseGetSerialNumber) response;
+				System.out.println("Received Response: " + number.serialNumber);
+			}
 
-                @Override
-                public ResponseTypes[] getListenerTypes() {
-                    return new ResponseTypes[]{ResponseTypes.LAN_GET_SERIAL_NUMBER_RESPONSE};
-                };
-    		});
-    	}
-    	z21.sendActionToZ21(new Z21ActionGetSerialNumber());
-    }
+			@Override
+			public ResponseTypes[] getListenerTypes() {
+				return new ResponseTypes[]{ResponseTypes.LAN_GET_SERIAL_NUMBER_RESPONSE};
+			};
+		});
+		z21.sendActionToZ21(new Z21ActionGetSerialNumber());
+	}
 }
