892ef305febd5eb1325101fc136c3f29765308e9
Sven Bursch-Osewold <git@bursch.com>
2016-06-18 12:37:43 +0200

    Adding Read CV via RailCom

---
+++ src/z21Drive/testing/TestRailComCV.java
@@ -0,0 +1,72 @@
+package z21Drive.testing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import z21Drive.LocoAddressOutOfRangeException;
+import z21Drive.Z21;
+import z21Drive.actions.Z21ActionLANXCVPomReadByte;
+import z21Drive.actions.Z21ActionLanXCVRead;
+import z21Drive.actions.Z21ActionLanXTrackPowerOff;
+import z21Drive.actions.Z21ActionLanXTrackPowerOn;
+import z21Drive.broadcasts.BroadcastTypes;
+import z21Drive.broadcasts.Z21Broadcast;
+import z21Drive.broadcasts.Z21BroadcastListener;
+import z21Drive.responses.ResponseTypes;
+import z21Drive.responses.Z21ResponseLanXCVResult;
+import z21Drive.responses.Z21Response;
+import z21Drive.responses.Z21ResponseListener;
+
+/**
+ * Reads the CV Number of the Loco via RailCom
+ * @see z21Drive.Z21
+ */
+public class TestRailComCV implements Runnable{
+
+	ArrayList<Integer> cvs =  new ArrayList<Integer>(Arrays.asList(new Integer[]{1, 28, 29}));
+    public static void main(String[] args) {
+        //Start things up
+        new Thread(new TestRailComCV()).start();
+    }
+
+    public void run(){
+        final Z21 z21 = Z21.instance;
+        z21.sendActionToZ21(new Z21ActionLanXTrackPowerOn());
+        z21.addResponseListener(new Z21ResponseListener() {
+            @Override
+            public void responseReceived(ResponseTypes type, Z21Response response) {
+                if (type == ResponseTypes.LAN_X_CV_RESULT){
+                	Z21ResponseLanXCVResult bc = (Z21ResponseLanXCVResult) response;
+                    String o = Integer.toBinaryString(bc.getValue());
+                    while (o.length() < 8) {
+                    	o = "0" + o;
+                    }
+                    System.out.println(String.format("%3d: %3d %s", bc.getCVadr(), bc.getValue(), o));
+                    sendNext(z21);
+                } else if (type == ResponseTypes.LAN_X_CV_NACK){
+                	System.out.println("Read CV failed.");
+                }
+            }
+
+            @Override
+            public ResponseTypes [] getListenerTypes() {
+                return new ResponseTypes[]{ResponseTypes.LAN_X_CV_RESULT, ResponseTypes.LAN_X_CV_NACK};
+            }
+        });
+        sendNext(z21);
+    }
+
+	private void sendNext(Z21 z21) {
+		if (cvs.size() == 0) {
+			z21.sendActionToZ21(new Z21ActionLanXTrackPowerOn());
+			System.exit(1);
+		}
+		Integer cv = cvs.get(0);
+		cvs.remove(0);
+        try {
+			z21.sendActionToZ21(new Z21ActionLANXCVPomReadByte(2, cv));
+		} catch (LocoAddressOutOfRangeException e) {
+			e.printStackTrace();
+		}
+	}
+}
