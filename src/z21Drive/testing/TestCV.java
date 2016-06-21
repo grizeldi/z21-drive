67270d641c495d94a380f0c5f2cf9a8a91549920
Sven Bursch-Osewold <git@bursch.com>
2016-06-21 16:04:06 +0200

    Remove unneeded Imports

--- src/z21Drive/testing/TestCV.java
+++ src/z21Drive/testing/TestCV.java
@@ -8,12 +8,9 @@ import z21Drive.Z21;
 import z21Drive.actions.Z21ActionLanXCVRead;
 import z21Drive.actions.Z21ActionLanXTrackPowerOff;
 import z21Drive.actions.Z21ActionLanXTrackPowerOn;
-import z21Drive.broadcasts.BroadcastTypes;
-import z21Drive.broadcasts.Z21Broadcast;
-import z21Drive.broadcasts.Z21BroadcastListener;
 import z21Drive.responses.ResponseTypes;
-import z21Drive.responses.Z21ResponseLanXCVResult;
 import z21Drive.responses.Z21Response;
+import z21Drive.responses.Z21ResponseLanXCVResult;
 import z21Drive.responses.Z21ResponseListener;
 
 /**
