633d38c248bb7867dd28bde25f5f819d0d7a31cf
Sven Bursch-Osewold <git@bursch.com>
2016-06-28 14:16:21 +0200

    Indent corrected

--- src/main/java/z21Drive/responses/Z21ResponseRailcomDatachanged.java
+++ src/main/java/z21Drive/responses/Z21ResponseRailcomDatachanged.java
@@ -11,36 +11,36 @@ import java.util.List;
 public class Z21ResponseRailcomDatachanged extends Z21Response {
 
 	private List<RailComData> data = new ArrayList<RailComData>();
-	
-    public Z21ResponseRailcomDatachanged(byte[] initArray) {
-        super(initArray);
-        boundType = ResponseTypes.LAN_RAILCOM_DATACHANGED;
-        if (byteRepresentation != null)
-            populateFields();
-    }
 
-    private void populateFields(){
-    	int nrOfDecoders = ((getByteRepresentation()[0] & 0xFF) - 4) / 13; 
-    	for (int idx = 0; idx < nrOfDecoders; idx++) {
-    		data.add(analyse(4 + idx * 13));
-    	}
-    }
+	public Z21ResponseRailcomDatachanged(byte[] initArray) {
+		super(initArray);
+		boundType = ResponseTypes.LAN_RAILCOM_DATACHANGED;
+		if (byteRepresentation != null)
+			populateFields();
+	}
+
+	private void populateFields(){
+		int nrOfDecoders = ((getByteRepresentation()[0] & 0xFF) - 4) / 13; 
+		for (int idx = 0; idx < nrOfDecoders; idx++) {
+			data.add(analyse(4 + idx * 13));
+		}
+	}
 
 	private RailComData analyse(int i) {
 		Integer[] data = new Integer[13];
 		for (int idx = i; idx < (i + 13); idx++) {
-//            System.out.print("0x" + String.format("%02X ", byteRepresentation[idx]));
-            data[idx - i] = (int) ( byteRepresentation[idx] & 0xFF);
+			//            System.out.print("0x" + String.format("%02X ", byteRepresentation[idx]));
+			data[idx - i] = (int) ( byteRepresentation[idx] & 0xFF);
 		}
 
 		int locId = data[0] 
-				    + (data[1]  << 8);
+				+ (data[1]  << 8);
 		int receiveCounter = data[2] 
-             			   + (data[3] << 8)
-							+ (data[4] << 16)
-							+ (data[5] << 24);
+				+ (data[3] << 8)
+				+ (data[4] << 16)
+				+ (data[5] << 24);
 		int errorCounter = data[6] 
- 			    + (data[7]<< 8)
+				+ (data[7]<< 8)
 				+ (data[8] << 16)
 				+ (data[9]<< 24);
 		int speed = data[10];
@@ -49,6 +49,11 @@ public class Z21ResponseRailcomDatachanged extends Z21Response {
 		return new RailComData(locId, receiveCounter, errorCounter, speed, options, temp);
 	} 
 
+	/**
+	 * Class for the Railcom-Data
+	 * @author sven
+	 *
+	 */
 	public class RailComData {
 		public RailComData(int locId, int receiveCounter, int errorCounter, int speed, int options, int temp) {
 			super();
@@ -83,13 +88,13 @@ public class Z21ResponseRailcomDatachanged extends Z21Response {
 		public int getTemp() {
 			return temp;
 		}
-		
+
 		@Override
 		public String toString() {
 			return "RailComData [locId=" + locId + ", receiveCounter=" + receiveCounter + ", errorCounter="
 					+ errorCounter + ", speed=" + speed + ", options=" + options + ", temp=" + temp + "]";
 		}
-		
+
 	}
 
 }
